95a5539badbbf69db36cc6e0bea0a7bdbd1cf7e8
yflorentz <fyaron@campus.technion.ac.il>
2024-12-09 11:34:54 +0200

    ex2

--- ex2.asm
+++ ex2.asm
@@ -3,12 +3,21 @@
 .section .text
 _start:
 #your code here
-movq (Address), %rsi
+movq $0, %rsi
+moveq $0, %rax
 
 movl (Length), %edi
 
 movb (Type), %cl
 movzx %cl, %rcx
 
-cells_loop_HW1:
 
+array_loop_HW1:
+cmp $0, %edi
+jle end_HW1
+cells_loop_HW1:
+add %rcx, %rsi
+movb Address(%rax, %rsi), %dl
+movzx %dl, %rdl
+
+end_HW1:
