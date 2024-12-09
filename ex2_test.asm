68fc9bf59baae1a965a96bb8be821a7d8cf262b4
yflorentz <fyaron@campus.technion.ac.il>
2024-12-09 09:46:57 +0200

    init

---
+++ ex2_test.asm
@@ -0,0 +1,30 @@
+
+.section .text
+
+  mov $result, %rax
+  cmpl $1, (%rax)
+  jne bad_exit
+
+  movq $60, %rax
+  movq $0, %rdi
+  syscall
+
+bad_exit:
+  movq $60, %rax
+  movq $1, %rdi
+  syscall
+
+.section .data
+
+Address:
+    .quad Array
+
+Type:
+	.byte 2
+
+Length:
+	.int 8
+
+Array:
+    .word 0x0001, 0x0002, 0x0003, 0x0004
+    .word 0x0005, 0x0006, 0x0007, 0x0008
