b4890276e51f62e49c9542a7af681334029a915b
yflorentz <fyaron@campus.technion.ac.il>
2024-12-10 16:14:12 +0200

    ex3

---
+++ ex5_test.asm
@@ -0,0 +1,23 @@
+.global _start
+
+.section .text
+  movb __ExpectedResult, %al
+  cmpb %al, result
+  jne bad_exit
+
+  movq $60, %rax
+  movq $0, %rdi
+  syscall
+
+bad_exit:
+  movq $60, %rax
+  movq $1, %rdi
+  syscall
+
+.section .data
+command: .asciz "int x=17"
+__Garbage_310385443: .byte FF, FF
+__ExpectedResult: .byte 1
+__Garbage_16686264: .byte 147, 159, 29, 204, 213, 203, 210, 118, 212
+result: .byte 0
+__Garbage_643585055: .byte 36
