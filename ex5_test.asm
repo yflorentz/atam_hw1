5c373ea4ff6dd000a1d0498c18d45bce8a695a1f
yflorentz <fyaron@campus.technion.ac.il>
2024-12-10 16:20:23 +0200

    ex5

--- ex5_test.asm
+++ ex5_test.asm
@@ -15,7 +15,7 @@ bad_exit:
   syscall
 
 .section .data
-command: .asciz "int x=17"
+command: .asciz "dkfjhjdkfjlsdkfjlsdkfj"
 __Garbage_310385443: .byte 0xFF, 0xFF
 __ExpectedResult: .byte 1
 __Garbage_16686264: .byte 147, 159, 29, 204, 213, 203, 210, 118, 212
