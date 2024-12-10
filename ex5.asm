76cb1af2998a06953f867b74cc63acbd6565921e
yflorentz <fyaron@campus.technion.ac.il>
2024-12-10 17:47:43 +0200

    ex5

--- ex5.asm
+++ ex5.asm
@@ -6,15 +6,104 @@ _start:
 #0x20= ' ', 0x3d = "=". 0x28 = '(', 0x29 = ')'
 
 movq $command, %rax
-no_space_loop_HW1:
-movb (%rax), %bl
-cmp $0, %bl
-jz success_HW1
+movq $0, %rcx #how many spaces
 
-inc %rax
+no_space_loop_HW1:
+	movb (%rax), %bl
+	cmp $0, %bl
+	jz success_HW1 #no_spaces
+	
+	cmp 0x20, %bl
+	je space_next_char_HW1
+
+	inc %rax
 jmp no_space_loop_HW1
 
+
+space_next_char_HW1:
+	inc %rcx
+	space_next_char_loop_HW1:	
+		inc %rax
+		movb (%rax), %bl
+
+		cmp 0x20, %bl
+		jne equal_loop_HW1
+
+		cmp $0, %bl
+		jz open_b_HW1 #spaces and no '='
+
+	jmp space_next_char_loop_HW1
+	
+	
+equal_loop_HW1:
+	cmp $2, %rcx
+	
+	jge keep_going_HW1
+	movq %rax, %rdx #for later use, first char after first space
+
+	keep_going_HW1:
+	je equal_third_part_HW1
+	
+	#we are in the 2nd part of the word
+	movb (%rax), %bl
+
+	cmp 0x3d, %bl
+	je equal_sign_exist_HW1
+
+	cmp 0x20, %bl
+	je space_next_char_HW1
+
+	cmp $0, %bl
+	jz open_b_HW1 #spaces and no '='
+
+	inc %rax
+jmp equal_loop_HW1
+
+
+equal_third_part_HW1:
+	cmp 0x3d, %bl
+	je equal_sign_exist_HW1
+	jmp open_b_HW1 #3rd part and it's not =
+
+equal_sign_exist_HW1:
+
+	after_equal_loop_HW1:
+		inc %rax
+		movb (%rax), %bl
+
+		cmp 0x20, %bl
+		je after_equal_loop_HW1
+	
+	after_equal_after_space_loop_HW1:
+		cmp $0, %bl
+		jz success_HW1 #no_spaces after '='
+		cmp 0x20, %bl
+		je open_b_HW1 #more then 1 part after '='
+
+		inc %rax
+		movb (%rax), %bl
+	jmp after_equal_after_space_loop_HW1
+
+open_b_HW1:
+	movb (%rdx), %bl
+	cmp $0x28, %bl
+	jne end_HW1 #failed all tests
+
+	open_b_loop_HW1:
+		inc (%rdx)
+		movb (%rdx), bl
+
+		cmp $0, %bl
+		jne open_b_loop_HW1
+
+	dec (%rdx)
+	movb (%rdx), %bl
+	cmp $0x29, %bl
+	je success_HW1 #end with )
+	jne end_HW1 #failed () test
+
 success_HW1:
-movb $1, (result)
+	movb $1, (result)
+	jmp end_HW1:
 
 end_HW1:
